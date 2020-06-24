bd17abf5350be24aaecfe5b1af8db1e95fef02e5
abc8 <2216944715@qq.com>
2020-06-24 11:40:30 +0800

    java 联系2

--- src/com/java/Test1.java
+++ src/com/java/Test1.java
@@ -2,6 +2,6 @@ package com.java;
 
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("111111111111111111111111111");
+        System.out.println("55555555555555555555555555");
     }
 }
