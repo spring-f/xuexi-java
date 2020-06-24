090dcdf7d02b806cff534b7e2abeb90253461554
abc8 <2216944715@qq.com>
2020-06-24 14:05:23 +0800

    java 联系4

--- src/com/java/Test1.java
+++ src/com/java/Test1.java
@@ -4,5 +4,16 @@ public class Test1 {
     public static void main(String[] args) {
         String a="SZZZZZZZZZZZZ";
         System.out.println(a);
+
+        String b="4654sa撒娇死哦静安寺骄傲是";
+        System.out.println(b);
+
+    }
+
+    public void sjia(){
+        System.out.println("老大发的");
+        System.out.println("老大发的");
+        System.out.println("老大发的");
+        System.out.println("老大发的");
     }
 }
