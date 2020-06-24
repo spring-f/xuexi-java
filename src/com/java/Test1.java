80176385b2c35b0341e7b6b5745a9c3ceee8dcc3
刘哥 <59285478+spring-f@users.noreply.github.com>
2020-06-24 11:49:48 +0800

    Update Test1.java

--- src/com/java/Test1.java
+++ src/com/java/Test1.java
@@ -4,4 +4,7 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("5555飒飒按上述所所所所所所所所所所5555555555555555555555");
     }
+    pubblic void sts(){
+        System.out.println("5555飒飒按上述所所所所所所所所所所5555555555555555555555");
+    }
 }
