53ff4804442257617e77749da4d76a2b75bfe589
abc8 <2216944715@qq.com>
2020-06-24 14:45:00 +0800

    java 联系4

---
+++ src/com/java/Test2.java
@@ -0,0 +1,17 @@
+package com.java;
+
+public class Test2 {
+    public static void main(String[] args) {
+
+
+    }
+
+    public void sjia(){
+        System.out.println("老大发的第二次次次次次次你对此" );
+        System.out.println("老大发的第二次次次次次次你对此" );
+        System.out.println("老大发的第二次次次次次次你对此" );
+        System.out.println("老大发的第二次次次次次次你对此" );
+        System.out.println("老大发的第二次次次次次次你对此" );
+
+    }
+}
