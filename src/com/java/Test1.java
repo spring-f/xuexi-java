3cab494ed681b548105408a9944a7ff6675d3709
abc8 <2216944715@qq.com>
2020-06-24 13:25:44 +0800

    java 联系3

--- src/com/java/Test1.java
+++ src/com/java/Test1.java
@@ -2,9 +2,7 @@ package com.java;
 
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("5555飒飒按上述所所所所所所所所所所5555555555555555555555");
-    }
-    pubblic void sts(){
-        System.out.println("5555飒飒按上述所所所所所所所所所所5555555555555555555555");
+        int a=45666666;
+        System.out.println(a);
     }
 }
